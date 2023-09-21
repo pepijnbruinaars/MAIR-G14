40a654717ab04ae9eeab623c8e2efba28fd11973
Pepijn Bruinaars <pepijnbruinaars@xs4all.nl>
2023-09-21 12:31:43 +0200

    Check if model is present, throw error otherwise

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
+import os
 from dialog_manager import DialogManager, DialogConfig
 
 
 def main():
+    # Check models folder for first time use
+    with os.scandir("models") as folder:
+        # If folder contains optimized_random_forest.joblib, then we are good to go
+        if "optimized_random_forest.joblib" in [file.name for file in folder]:
+            pass
+        # Train model if not
+        else:
+            raise Exception(
+                "Intent classification model not found. Please run the random_forest.py script to train the model."
+            )
+
     dialog_config: DialogConfig = {"intent_model": "RandomForest", "verbose": True}
 
     dialog_manager = DialogManager(dialog_config)
